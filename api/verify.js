3fc4eeede3cd820f039c33c81d01b7ed5a538ed5
mrcharlesbrown <144152379+mrcharlesbrown@users.noreply.github.com>
2026-02-25 14:15:20 +0800

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,38 +1,14 @@
-/**
- * MCB LICENSE LOCAL
- * File: https://github.com/mrcharlesbrown/mcb-license-local/api/verify.js 
- * Raw: https://raw.githubusercontent.com/mrcharlesbrown/mcb-license-local/main/api/verify.js
- * 
- * Handles Priviledges, Integrity Check, Verification, Deactivation, etc. For Offline Validation
- * 
- * Note: user shorter const for public files
- * 
- * @version 1.0.0
- * @author mrcharlesbrown [mcbdesk@hotmail.com]
- * @copyright All Rights Reserved.
- * 
- */
-function verify() {
-	const localAP = true; // SECURITY SWITCH: Set to true to enable priviledges from the client. Keep false for production.
-	const priviledgeProductId = ['MCB_Fileslync']; // Only allow specified customProductId to authorize priviledges. If false or [], allow all apps to use priviledge.
+{
+      "name": "MCB LICENSE LOCAL",
+      "file": "https://github.com/mrcharlesbrown/mcb-license-local/api/verify.js",
+      "raw": "https://raw.githubusercontent.com/mrcharlesbrown/mcb-license-local/main/api/verify.js",
+      "task": "Handles Priviledges, Integrity Check, Verification, Deactivation, etc. For Offline Validation",
+      "version": "1.0.0",
+      "author": "mrcharlesbrown [mcbdesk@hotmail.com]",
+      "copyright": "All Rights Reserved.",
 
-	// Expose API
-	return {
-		localAP: localAP,
-		priviledgeProductId: priviledgeProductId,
-	}
-};
-return verify();
-
-
-
-
-
-
-
-
-
-
-
-
-
+      "localAP": true,
+      "priviledgeProductId":[
+            "MCB_Fileslync"
+      ]
+}
