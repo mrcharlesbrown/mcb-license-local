0a1550aabcf8104d5cd5a34ed9e47a32c7b907ae
mrcharlesbrown <144152379+mrcharlesbrown@users.noreply.github.com>
2026-03-25 00:12:08 +0800

    Add files via upload

---
+++ public/updates.js
@@ -0,0 +1,38 @@
+const api = {
+      // ================================================
+      "name": "MCB UPDATES LOCAL (Logs, News, Announcements)",
+      "file": "https://github.com/mrcharlesbrown/mcb-license-local/blob/main/public/updates.js",
+      "raw": "https://raw.githubusercontent.com/mrcharlesbrown/mcb-license-local/main/public/updates.js",
+      "task": "Handles updates, news, announcements, changelogs, and other related information for MCB License Local. This can be used to inform users about new features, bug fixes, important notices, and other relevant updates regarding the software.",
+      "version": "1.0.0",
+      "author": "mrcharlesbrown [mcbdesk@hotmail.com]",
+      "copyright": "All Rights Reserved.",
+      // ================================================
+
+      // Green - General Announcements, Blue - News/Updates/logs, Yellow - Warnings, Red - Errors
+      "announcements": { 
+            "2026-05-01": "Welcome to MCB Application! This is the initial release. Stay tuned for updates, new features, and improvements. Thank you for your support!",
+            "2026-05-01": "Go to License tab to activate your license key and access all features. If you encounter any issues, please contact support at mcbdesk@hotmail.com"
+      },
+
+      // ================================================
+      "logs": {
+            // "2026-05-02": "User 'john_doe' activated a license key successfully. License Key: LICENSE-KEY-4. Activation Time: 2026-06-04 10:15:30."
+      },
+      "news": {
+            // "2026-05-03": "We are excited to announce that we are working on a new feature that will allow users to manage their licenses more efficiently. Stay tuned for more updates!"
+      },
+
+      // ================================================
+      "warnings": {
+            "2026-05-04": "Please ensure that you are using the latest version of this application to avoid any potential issues. Older versions may not be supported and could lead to activation problems."
+      },
+
+      // ================================================
+      "errors": {
+            // "2026-05-05": "We are aware of an issue affecting some users during activation. Our team is actively investigating and working on a fix. We apologize for any inconvenience caused and appreciate your patience."
+      },
+
+
+}
+
